15b9b6dbe974893e586ade9d82444ae1026480b4
YankoBuyens <Yanko.Buyens@Student.ucll.be>
2021-04-01 19:18:08 +0200

    Help

---
+++ src/views/CancelCountry.vue
@@ -0,0 +1,14 @@
+<template>
+  <input v-model="countryString" placeholder="edit me">
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      countryString: this.countryString,
+      previousValue: '',
+    }
+  }
+}
+</script>
